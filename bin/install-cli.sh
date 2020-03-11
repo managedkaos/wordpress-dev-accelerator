defc279c0a3af94fd040022bbdffe112cb565b55
Michael <michael@managedkaos.com>
2020-03-10 22:37:15 -0700

    Remove install stuff

--- bin/install-cli.sh
+++ bin/install-cli.sh
@@ -8,18 +8,3 @@ echo "Installing WP-CLI..."
     https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x /usr/local/bin/wp
-
-/usr/local/bin/wp --info
-
-echo "Installing Wordpress..."
-/usr/local/bin/wp core install \
-    --url=localhost:13000 \
-    --title="WP Development Site" \
-    --admin_user=admin \
-    --admin_password=admin \
-    --admin_email=admin@example.com \
-    --path=/var/www/html
-
-echo "Removing default plugins..."
-./wp plugin delete hello
-./wp plugin delete akismet
