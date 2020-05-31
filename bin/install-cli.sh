3826a7d711657d1e595274e40c72b19a15d59694
Michael <michael@managedkaos.com>
2020-05-30 17:40:09 -0700

    mkdir /var/www/.wp-cli/ and chown to www-data

--- bin/install-cli.sh
+++ bin/install-cli.sh
@@ -8,3 +8,5 @@ echo "Installing WP-CLI..."
     https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x /usr/local/bin/wp
+mkdir -p /var/www/.wp-cli/
+chown -R www-data:www-data /var/www/.wp-cli/
